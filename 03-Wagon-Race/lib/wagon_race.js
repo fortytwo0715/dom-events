061bf5cd891601463bca16e18408cc3043f0e9ea
Weijing Zhu <fortytwo0715@gmail.com>
2019-10-31 22:37:56 +0800

    Mini race game using event listening

--- 03-Wagon-Race/lib/wagon_race.js
+++ 03-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,43 @@
 // TODO: write your code here
+let player1path = document.querySelectorAll("#player1_race td")
+let player2path = document.querySelectorAll("#player2_race td")
+
+let x=0;
+let y=0;
+
+document.addEventListener('keyup', function(event) {
+   if (event.code == 'KeyP') {
+
+        if (player1path[x].className == "active") {
+        player1path[x].classList.remove("active");
+        player1path[x+1].classList.add("active");
+        x++;
+        }
+    };
+
+
+   if (event.code == 'KeyQ') {
+
+        if (player2path[y].className == "active") {
+        player2path[y].classList.remove("active");
+        player2path[y+1].classList.add("active");
+        y++;
+        }
+    };
+
+    if (x == 11 && x > y) {
+    alert("Player 1 Wins!");
+    location.reload();
+}
+    if (y == 11 && x < y) {
+    alert("Player 2 Wins!");
+    location.reload();
+}
+
+  let btn = document.querySelector("button")
+  btn.addEventListener("click", (event) => {
+  location.reload();
+});
+ }
+);
+
